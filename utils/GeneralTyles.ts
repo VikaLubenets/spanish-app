0e02e37dbec3d4fc8e337418713f5d10343565f1
Vika Lubenets <lubenetcvika@gmail.com>
2023-12-04 16:37:38 +0400

    feat: add mock data

---
+++ utils/GeneralTyles.ts
@@ -0,0 +1,22 @@
+export enum TensesIndicativo {
+  INFINITIVE = 'Infinitivo',
+  PRESENT = 'Presente',
+  PRETERITE = 'Pretérito Indefinido',
+  IMPERFECT = 'Pretérito Imperfecto',
+  COMPUESTO = 'Pretérito Perfecto (Compuesto)',
+  PLUSQUAMPERFECTO = 'Pretérito Plusquamperfecto',
+  ANTERIOR = 'Pretérito Anterior',
+  FUTURE = 'Futuro',
+  FUTURE_PERFECTO = 'Futuro Perfecto',
+  CONDITIONAL = 'Conditional',
+  COMMAND_AFFIRMATIVE = 'Imperativo Afirmativo',
+  COMMAND_NEGATIVE = 'Imperativo Negativo',
+}
+
+export enum TensesSubjuntivo {
+  PRESENT = 'Presente de Subjunctivo',
+  IMPERFECT = 'Pretérito Imperfecto de Subjunctivo',
+  COMPUESTO = 'Pretérito Perfecto (Compuesto) de Subjunctivo',
+  PLUSQUAMPERFECTO = 'Pretérito Plusquamperfecto de Subjunctivo',
+  FUTURE = 'Future de Subjunctivo',
+}
